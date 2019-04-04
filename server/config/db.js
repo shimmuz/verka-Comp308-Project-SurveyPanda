9457594619a8686ee409bdcd129ba32e36a7fb8f
Shefali Sharma <shefalisharmaspd@gmail.com>
2019-04-03 22:28:32 -0300

    mlab added

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
    //"URI" : "mongodb://localhost/test",
-    "URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
+    // "URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
+    "URI" : "mongodb://shefali:a123456@ds058548.mlab.com:58548/shefali",
     "secret": "SomeSecret"
 }
